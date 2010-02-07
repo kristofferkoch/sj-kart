dd96a090c6cd285cac26d00e15c2ff3526aed0c0
Kristoffer Ellersgaard Koch <kristoffer.koch@gmail.com>
2010-02-07 17:15:52 +0100

    Made menu and user-interface nicer

--- bruker.js
+++ bruker.js
@@ -1,118 +1,163 @@
+var user = {};
 (function(){
-	var menuDiv, loginSpan, menu, user;
+	var loginDiv, userData;
 
-	menu = SPAN(null, " ", A({href: "#legg_til"}, "Legg til..."));
-
+	var setFormEnabled = function(en) {
+		kart.setKeyboardControlEnabled(!en);
+		var form = loginDiv.getElementsByTagName("form")[0];
+		for(var i=0; i < form.elements.length; i++) {
+			form.elements[i].disabled = !en;
+		}
+	};
 	var jsonCb = function(err, r) {
 		r = evalJSONRequest(r);
 		authCallback(r, err);
 	};
-	var doLogin = function(username, password) {
+	user.doLogin = function(form) {
+		var username, password;
+		username = form.elements[0].value;
+		password = form.elements[1].value;
+		if (username.length === 0 || password.length === 0) {
+			status.set("Oppgi brukernavn og passord.", 5);
+			return;
+		}
+		status.set("Logger inn " + username + "...");
+		setFormEnabled(false);
 		var d = doXHR("auth.php", {
 					method: "POST",
 					sendContent:queryString({'user': username, 'password': password}),
 					headers:{"Content-Type": "application/x-www-form-urlencoded"}
 				});
-		d.addCallbacks(partial(jsonCb, true), showLoginForm);
+		d.addCallback(partial(jsonCb, true));
 	};
-	
-	var doLogout = function() {
-		var d = doXHR("auth.php", {
-					method: "POST",
-					sendContent: queryString({"logout": true}),
-					headers:{"Content-Type": "application/x-www-form-urlencoded"}
-				});
-		user = undefined;
-		clearLoginForm();
-		d.addCallbacks(partial(jsonCb, false), showLoginForm);
-		return false;
+	user.cancelLogin = function(form) {
+		loginDiv.style.display = "none";
+		setFormEnabled(false);
 	};
-	
-	var showLoginForm, clearLoginForm;
-	(function() {
-		var loggingIn = SPAN(null, "Logger inn...");
-		
-		var formSubmitted = function() {
-			kart.setKeyboardControlEnabled(true);
-			swapDOM(loginSpan, loggingIn);
-			loginSpan = loggingIn;
-			doLogin(usernameInput.value, passwordInput.value);
-			return false;
-		};
-		var usernameInput = INPUT({type:"text"},""),
-			passwordInput = INPUT({type:"password"}, ""),
-			form = FORM({onsubmit:formSubmitted},
-					usernameInput, passwordInput,
-					INPUT({type:"submit", value:"Logg inn"}),
-					INPUT({type:"reset", value:"Avbryt", 
-							onclick: function() {
-									kart.setKeyboardControlEnabled(true);
-									showLoginLink();
-									return true;
-								}
-						})
-				);
 
-		showLoginForm = function() {
-			// turn off map keyboard-navigation
-			kart.setKeyboardControlEnabled(false);
-			swapDOM(loginSpan, form);
-			loginSpan = form;
-			usernameInput.focus();
-			return false;
-		};
-		clearLoginForm = function() {
-			form.reset();
-		}
-	})();
-	
-	var showLogoutForm = function() {
-		var n = SPAN(null, "Hei, "+user.name+"! ",
-					A({href:"#logout", onclick:doLogout}, "Ikke deg?")
-					);
-		swapDOM(loginSpan, n);
-		loginSpan = n;
-	};
-	
-	var showLoginLink = function() {
-		var n = SPAN(null, A({href:"#login", onclick:partial(showLoginForm, "")},
-								"Logg inn."), " ",
-							A({href:"xxx"}, "Register bruker")
-					);
-		swapDOM(loginSpan, n);
-		loginSpan = n;
-	};
 
 	var authCallback = function(result, err) {
 		var n;
 		if (result === "no") {
-			user = undefined;
-			showLoginLink();
+			status.clear();
+			loginDiv.style.display = "none";
+			menu.showLoginLink();
 		} else if (result.id && result.name) {
-			user = result;
-			showLogoutForm();
+			status.set("Logget inn som " + result.name + ".", 5);		
+			loginDiv.style.display = "none";
+			menu.showUser(result);
+		} else if (result === "logout") {
+			status.set("Logget ut.", 5);
+			setFormEnabled(false);
+			loginDiv.style.display = "none";
+			menu.showLoginLink();
 		} else {//if (result === "fail") {
-			user = undefined;
 			if (err) {
-				kart.showMessage("Feil brukernavn eller passord");
-				showLoginForm();
+				setFormEnabled(true);
+				status.set("Feil brukernavn eller passord", 5);
+				shake(loginDiv);
+				var f = loginDiv.getElementsByTagName("input")[1];
+				f.select();
+				f.focus();
 			} else {
-				showLoginLink();
+				status.clear();		
+				loginDiv.style.display = "none";
+				menu.showLoginLink();
 			}
 		}
 	};
 	
+	var menu = (function() {
+		var div = $("menu");
+		var current = SPAN({'class':"loading"}, "Laster...");
+		var showLogin = function() {
+			setFormEnabled(true);
+			loginDiv.style.display="block";
+			loginDiv.getElementsByTagName("input")[0].focus();
+			return false;
+		};
+		var logout = function () {
+			var d = doXHR("auth.php", {
+						method: "POST",
+						sendContent: queryString({"logout": true}),
+						headers:{"Content-Type": "application/x-www-form-urlencoded"}
+					});
+			d.addCallback(partial(jsonCb, false));
+			status.set("Logger ut...");
+			loginDiv.getElementsByTagName("input")[1].value = "";
+			return false;
+		};
+		var loginLink = A({href:"#login", onclick:showLogin}, "Logg inn");
+		appendChildNodes(div, " ", current);
+		return {
+			showLoginLink: function() {
+				div.removeChild(current);
+				appendChildNodes(div, loginLink);
+				current = loginLink;
+			},
+			showUser: function(user) {
+				var n = SPAN(null, "Logget på som "+user.name+". ",
+							A({href:"#logout", onclick: logout}, "Logg ut.")
+						);
+				div.removeChild(current);
+				appendChildNodes(div, n);
+				current = n;
+			}
+		};
+	}); /* initialized by init() */
+	
+	var status = (function() {
+		var div = $("status");
+		var vis = true;
+		var to = undefined;
+		var clear = function() {
+			if (to) {
+				to.cancel();
+				to = undefined;
+			}
+			replaceChildNodes(div, "");
+			if (vis) {
+				blindUp(div);
+				//toggle(div);
+				vis = false;
+			}
+		};
+		return {
+			set: function(msg, timeout) {
+				if (to !== undefined) {
+					to.cancel();
+					to = undefined;
+				}
+				if (vis) {
+					appendChildNodes(div, BR(), msg);	
+				} else {
+					replaceChildNodes(div, msg);
+					div.style.display="block";
+					//blindDown(div, options);
+					vis = true;
+				}
+				if (timeout) {
+					to = callLater(timeout, clear);
+				}
+			},
+			clear: clear
+		};
+	});
+
 	var init = function() {
 		var d;
-		menuDiv = document.getElementById("menu");
-
-		loginSpan = SPAN("Sjekker innlogging...");
-		menuDiv.removeChild(menuDiv.childNodes[0]);
-		menuDiv.appendChild(loginSpan);
-		menuDiv.appendChild(menu);
+		loginDiv = $("login");
+		
+		status = status();
+		menu = menu();
+		
+		status.set("Sjekker innlogging...");
 		
 		d = loadJSONDoc("auth.php");
-		d.addCallbacks(authCallback, partial(alert, "fail"));
+		d.addCallback(function(res) {
+			status.clear();
+			authCallback(res);
+		});
 	};
 	addOnLoad(init);
 })();
