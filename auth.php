240a24571ca0d5f61fa7121f1d183869babfcc4b
Kristoffer Ellersgaard Koch <kristoffer.koch@gmail.com>
2010-02-07 17:15:21 +0100

    Integrated authentication closer with mediawiki

--- auth.php
+++ auth.php
@@ -1,12 +1,16 @@
 <?php
 header('Content-type: application/javascript');
 
+
 chdir("/usr/share/mediawiki/");
 require_once("/usr/share/mediawiki/includes/WebStart.php");
 require_once("/usr/share/mediawiki/includes/Exception.php");
 require_once("/usr/share/mediawiki/includes/User.php");
+require_once("/usr/share/mediawiki/includes/Setup.php");
 
-session_start();
+#echo "/*\n";
+#var_dump($_SESSION);
+#echo "*/\n";
 
 function userData($user) {
 	echo json_encode(array(
@@ -16,33 +20,31 @@ function userData($user) {
 
 }
 
+
 /* User logging in with POST? */
 if (isset($_POST["user"]) && isset($_POST["password"])) {
 	$user = User::newFromName($_POST["user"]);
 	if ($user && $user->checkPassword($_POST["password"]) && $user->getId() !== 0) {
 		/* login ok */
-		$_SESSION["userid"] = $user->getId();
-		$_SESSION["username"] = $user->getName();
+		$_SESSION["wsUserID"] = $user->getId();
+		$_SESSION["wsUserName"] = $user->getName();
 		userData($user);
 	} else {
 		/* login failed */
 		echo json_encode("fail");
 	}
 /* User logging in with session? */
-} else if (isset($_SESSION["userid"]) && $_SESSION["userid"] !== 0) {
-	if (isset($_POST["logout"])) {
-		unset($_SESSION["userid"]);
-		unset($_SESSION["username"]);
-		echo json_encode("logout");
-	} else {
-		$user = User::newFromId($_SESSION["userid"]);
-		if ($user) {
-			userData($user);
-		} else {
-			echo json_encode("fail");
-		}
-	}
 } else {
-	echo json_encode("no");
+	$user = User::newFromSession();
+	if ($user->getId() !== 0) {
+		if (isset($_POST["logout"])) {
+			$user->logout();
+			echo json_encode("logout");
+		} else {
+			userData($user);
+		}
+	} else {
+		echo json_encode("no");
+	}
 }
 ?>
