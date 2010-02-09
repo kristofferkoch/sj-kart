b170ca3b7c4a0b975e6d7ef8d24cae6bd9254050
Kristoffer Ellersgaard Koch <kristoffer.koch@gmail.com>
2010-02-09 09:42:25 +0100

    Some search framework

--- search.js
+++ search.js
@@ -4,11 +4,15 @@ SEARCH = {};
 	SEARCH.doSearch = function(form) {
 		var str = form.elements[0].value;
 		var d = loadJSONDoc("search.php?search="+escape(str));
-		d.addCallbacks(function(result) {
-			alert(serializeJSON(result));
-		}, function(error) {
-			alert("error: "+error);
+		d.addCallback(function(result) {
+			if (result.length === 0) {
+				STATUS.add("Ingen resultat for '"+str+"'.", 5);
+				return;
+			}
+			STATUS.add("Søk ikke ordentlig implementert enda", 6);
+			map(function(r) {STATUS.add(r.name + " " + r.position, 5);}, result);
 		});
+		STATUS.handleDeferred(d, "Søker...", null, "Søk feilet");
 		
 	};
 
